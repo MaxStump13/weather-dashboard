befcaa0a57203f2d8a144f26e33eb659ae015e8d
MaxStump13 <stumpmaximilian@gmail.com>
2021-12-17 10:08:20 -0500

    initial commit, added file structure, api key in var, scripts and stylesheets, and added pseudocode for tasks

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+var APIKey = "2024fa16c7d4ae9b96edf7810fbb56a4";
+var city;
+
+//use api to call for citys lon and lat then another call
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//add ?per_page=5 for the future forecast
+
+//need to make the input search bar a nested api call, then display of 5 cards for forecast and another nested api for the current, then displayed in a col/container or maybe a card as well
+
+//use bootstrap to make a left aside col that can expand -y for previous searches, these rows need to be clickable to the previous searches and stored locally or server side?
+
