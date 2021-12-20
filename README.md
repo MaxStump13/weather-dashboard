3e894465ebd2d365c1d8a3bf9fedd87e27ffbe18
MaxStump13 <stumpmaximilian@gmail.com>
2021-12-20 16:48:29 -0500

    added function for creating previous searches, made changes to css for visual improvement, added alts for icons, added comments, and formatted for easier reading

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # weather-dashboard
+
+## Description
+The goal was to create a website to display the current and 5-day forecast of a city. This weather dashboard takes in a user-searched city and displays the weather conditions from openweathermap's api. The weather dashboard stores all previous searches locally, and displays those cities as clickable buttons on the page. It works for cities in the US and various other countries. During the creation of this website, I have learned how to use the fetch API and then method to obtain data from third party APIS. I also got more practice using DOM traversal to get the wanted information from the fetch and then methods. 
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+- [Tests](#tests)
+
+## Installation
+N/A
+
+## Usage
+Here is a link to the deployed Weather Dashboard. [weather dashboard deployed link](https://maxstump13.github.io/weather-dashboard/)
+
+When the page is loaded, the dashboard will appear as seen below. Because there is no locally stored searches, the screen will mostly be empty.
+![first time loading page](./assets/images/first-time.png)
+
+Once the user searches for a city, the current and forecasted weather conditions will be displayed on the page. The icons associated with the weather will also be displayed. ![display of weather after search](./assets/images/search.png)
+
+Once a search has been made, the data is stored as a JSON object locally. 
+![local storage of city searched](./assets/images/local-storage.PNG)
+
+The user is able to check the weather and forecast in multiple countries around the world. Below are some examples. Notice the color surrounding the UV Index also changed. The UV Index is color coded to represent low to high values.
+![Multiple saved searches in previous searches and color change for UV index](./assets/images/uvi-color.png).
+
+If the user inputs a nonexistant city or a city not in the openweathermap database, an alert pops up informing the user of the error and error code. ![404 error for invalid city](./assets/images/404.PNG)
+
+Because the previous searches are stored locally, they will populate the page even if it is refreshed or closed. ![Page refresh still displays previous searches](./assets/images/reload.png)
+
+## Credits
+1. [MDN](https://developer.mozilla.org/en-US/)
+2. [Tutsplus](https://tutsplus.com/?_ga=2.189473323.1003266245.1639752011-1575437401.1638298985)
+3. [JQuery](https://jquery.com/)
+4. [Openweathermap](https://openweathermap.org/)
+5. [Moment](https://momentjs.com/)
+6. [Bootstrap](https://getbootstrap.com/)
+7. [W3](https://www.w3schools.com/)
+8. [Stackoverflow](https://stackoverflow.com/)
+
+## License
+[License](./LICENSE)
+
+## Badges
+N/A
+
+## Features
+1. Search box that takes user input and the Fetch API method for current/forecasted weather conditions
+2. Stores previous searches locally
+3. layout built with bootstrap
+4. Use of moment.js for current/forecast dates
+5. Previous searches are clickable buttons
+6. Works for cities world wide
+7. Various functions/methods to aid readibility
+
+## How to Contribute
+If there are any suggestions to improve this generator, this is my [GitHub](https://github.com/MaxStump13) account. 
+
+## Tests
+I tested the functionality of the weather dashboard in a few ways. I typed random text and received the 404 error alert. I also searched the same city multiple times, but because of the conditional statements, it was not saved a second time. I confirmed the UV Index span changes colors at a higher UV index. The buttons for the previous section all will work as well, with updated information. This is because on click they are sent to the getCoordinates function, not just the displayWeather function.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,214 +1,248 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// Global variables
+// Openweathermap API Key
 var APIKey = "2024fa16c7d4ae9b96edf7810fbb56a4";
-var storedCities=[];
+// empty array for storage
+var storedCities = [];
+// var for search form element
 var searchFormEl = document.querySelector("#search-form");
+// var for City name element in current weather container
 var cityNameEl = document.querySelector(".cityName");
+// var for string search input
 var searchInputVal = document.querySelector("#search-input");
+// Container element for current city weather
 var cityContainerEl = document.querySelector("#city-container");
+// container element for list of current city weather conditions
 var currCityContainerEl = document.querySelector("#curr-container");
+// element of current date in current city container
 var currDateEl = document.querySelector("#currDate");
+// element for weather icon in current city
 var currWeatherIconEl = document.querySelector("#weather-icon");
+//var for element flexbox container of the searched city weather/forecast
 var resultsEl = document.querySelector(".col-8");
+// var for element of forecast container
 var forecastEl = document.querySelector("#forecast-container");
-var prevEl =document.querySelector("#prevSearches");
+// var for previous searches element container
+var prevEl = document.querySelector("#prevSearches");
+// boolean used to test if a search has already been stored locally
 var filled = false;
-
-function handleSearchFormSubmit(event){
-    event.preventDefault();
-    var citySearch = searchInputVal.value.trim();
-    if(!citySearch){
-        console.error("need search input");
-    }
-    searchInputVal.value = "";
-    cityNameEl.innerText ="";
-    if(filled == true){
+// function to take in search input
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  // trims empty spaces on input
+  var citySearch = searchInputVal.value.trim();
+  // if blank error pops up
+  if (!citySearch) {
+    console.error("need search input");
+  }
+  // clears input field and previous cities names in current city container
+  searchInputVal.value = "";
+  cityNameEl.innerText = "";
+  // checks if current city container/forecast is populated, if true calls clearSpace function
+  if (filled == true) {
     clearSpace();
-    }
-    getCity(citySearch);
+  }
+  // call to getCity function
+  getCity(citySearch);
 }
-function getCity(city){
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    fetch(apiUrl)
-        .then(function(response){
-            if(response.ok){
-                console.log(response);
-                response.json().then(function(data){
-                    getCoordinates(data, city);
-                });
-            }else{
-                alert("error: "+ response.statusText);
-            }
-        })
-        .catch(function(error){
-            alert("unable to connect");
+// function to use search input to fetch api url, then get data from api if valid input
+function getCity(city) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  fetch(apiUrl)
+    // only procedes if status of api is ok, if not alerts the errorr
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          getCoordinates(data);
         });
-};
-function getCoordinates(info, searchedCity){
-    if(info.length===0){
-        cityContainerEl.textContent = "City not found";
-        return;
-    }
-    console.log(info);
-    cityNameEl.textContent = info.name;
-    var coordinates = info.coord;
-    var lon = coordinates.lon;
-    var lat = coordinates.lat;
-    var APIUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
-    fetch(APIUrl)
-        .then(function(response){
-            if(response.ok){
-                // console.log(response);
-                response.json().then(function(data){
-                    displayWeather(data, searchedCity);
-                    storeCity(info);
-                });
-            }else{
-                alert("error: "+ response.statusText);
-            }
-        })
-        .catch(function(error){
-            alert("unable to connect");
+      } else {
+        alert("error: " + response.statusText + " (" + response.status +")");
+      }
+    })
+    .catch(function (error) {
+      alert("unable to connect");
+    });
+}
+// function uses data from city api url to obtain coordinates for more data
+function getCoordinates(info) {
+  // checks/displays if no info on the city is in the api then returns out of the function
+  if (info.length === 0) {
+    cityContainerEl.textContent = "City not found";
+    return;
+  }
+  // sets City name in current city container and pulls coordinates for another fetch
+  cityNameEl.textContent = info.name;
+  var coordinates = info.coord;
+  var lon = coordinates.lon;
+  var lat = coordinates.lat;
+  var APIUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=minutely,hourly,alerts&units=imperial&appid=" +
+    APIKey;
+  fetch(APIUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+          storeCity(info);
         });
+      } else {
+        alert("error: " + response.statusText + " (" + response.status +")");
+      }
+    })
+    .catch(function (error) {
+      alert("unable to connect");
+    });
 }
-function displayWeather(locData,city){
-        resultsEl.classList.remove("hidden");
-        currDateEl.textContent =" (" + moment().format("l") +"):"; 
-        var iconId = locData.current.weather[0].icon;
-        var icon = "http://openweathermap.org/img/wn/" + iconId +"@2x.png";
-        currWeatherIconEl.setAttribute("src", icon);
-        var tempEl = document.createElement("li");
-        tempEl.textContent = "Temperature: " + locData.current.temp + "°F";
-        var windEl = document.createElement("li");
-        windEl.textContent = "Wind Speed: " + locData.current.wind_speed + "MPH";
-        var humidityEl = document.createElement("li");
-        humidityEl.textContent = "Humidity: " + locData.current.humidity + "%";
-        var uvEl = document.createElement("li");
-        var uvCond = document.createElement("span");
-        uvCond.textContent = locData.current.uvi;
-        if(locData.current.uvi<4){
-            uvCond.classList= "favor";
-        }else if(locData.current.uvi<7){
-            uvCond.classList= "mod";
-        }else{
-            uvCond.classList= "severe";
-        }
-        uvEl.textContent = "UV Index: ";
-        
-        for(i=1; i<6;i++){
-            var card = document.createElement("section");
-            card.classList = "card";
-            card.setAttribute("id","fore");
-            var date = document.createElement("h4")
-            date.classList = "card-header";
-            date.setAttribute("id", "foreDate");
-            date.textContent = moment(locData.daily[i].dt, "X").format("l");
-            var foreBody = document.createElement("section");
-            var foreTemp = document.createElement("p");
-            foreBody.classList = "card-body";
-            foreTemp.textContent = "Temp: " + locData.daily[i].temp.day + "°F";
-            var foreIconEl = document.createElement("img");
-            var foreIconID = locData.daily[i].weather[0].icon;
-            var foreIcon = "http://openweathermap.org/img/wn/" + foreIconID +"@2x.png";
-            foreIconEl.setAttribute("src", foreIcon);
-            foreIconEl.setAttribute("id", "forecastIcon");
-            foreTemp.textContent = "Temp: " + locData.daily[i].temp.day + "°F";
-            var foreWind = document.createElement("p");
-            foreWind.textContent = "Wind: " + locData.daily[i].wind_speed + " MPH";
-            var foreHumid = document.createElement("p");
-            foreHumid.textContent = "Humidity: " + locData.daily[i].humidity + "%";
-            foreBody.append(foreIconEl, foreTemp, foreWind, foreHumid);
-            card.append(date,foreBody);
-            forecastEl.append(card);
-        }
-        uvEl.append(uvCond);
-        cityNameEl.append(currDateEl, currWeatherIconEl);
-        currCityContainerEl.append(tempEl,windEl,humidityEl,uvEl);    
-        filled = true;
+// function to build and display current/forecasted weather conditions
+function displayWeather(locData) {
+  // removes hidden class
+  resultsEl.classList.remove("hidden");
+  // uses moment to set the current date in current city container
+  currDateEl.textContent = " (" + moment().format("l") + "):";
+  // gets icon id and links it to img element beside city name/date
+  var iconId = locData.current.weather[0].icon;
+  var iconDes = locData.current.weather[0].description;
+  var icon = "http://openweathermap.org/img/wn/" + iconId + "@2x.png";
+  currWeatherIconEl.setAttribute("src", icon);
+  currWeatherIconEl.setAttribute("alt", iconDes);
+  // gets current weather conditions and builds elements to display them in a list in current city container
+  var tempEl = document.createElement("li");
+  tempEl.textContent = "Temperature: " + locData.current.temp + "°F";
+  var windEl = document.createElement("li");
+  windEl.textContent = "Wind Speed: " + locData.current.wind_speed + "MPH";
+  var humidityEl = document.createElement("li");
+  humidityEl.textContent = "Humidity: " + locData.current.humidity + "%";
+  var uvEl = document.createElement("li");
+  var uvCond = document.createElement("span");
+  uvCond.textContent = locData.current.uvi;
+  // depending on UV Index value assigns class to color index from green to yellow to red
+  if (locData.current.uvi < 4) {
+    uvCond.classList = "favor";
+  } else if (locData.current.uvi < 7) {
+    uvCond.classList = "mod";
+  } else {
+    uvCond.classList = "severe";
+  }
+  uvEl.textContent = "UV Index: ";
+  // loop to create/display cards/conditions/spacing for 5 day forecast of weather conditions
+  for (i = 1; i < 6; i++) {
+    var card = document.createElement("section");
+    card.classList = "card";
+    card.setAttribute("id", "fore");
+    var date = document.createElement("h4");
+    date.classList = "card-header";
+    // date.setAttribute("id", "foreDate");
+    date.textContent = moment(locData.daily[i].dt, "X").format("l");
+    var foreBody = document.createElement("section");
+    var foreTemp = document.createElement("p");
+    foreBody.classList = "card-body";
+    foreTemp.textContent = "Temp: " + locData.daily[i].temp.day + "°F";
+    var foreIconEl = document.createElement("img");
+    var foreIconID = locData.daily[i].weather[0].icon;
+    var foreIconDesc = locData.daily[i].weather[0].description;
+    var foreIcon = "http://openweathermap.org/img/wn/" + foreIconID + "@2x.png";
+    foreIconEl.setAttribute("src", foreIcon);
+    foreIconEl.setAttribute("alt", foreIconDesc);
+    foreIconEl.setAttribute("id", "forecastIcon");
+    foreTemp.textContent = "Temp: " + locData.daily[i].temp.day + "°F";
+    var foreWind = document.createElement("p");
+    foreWind.textContent = "Wind: " + locData.daily[i].wind_speed + " MPH";
+    var foreHumid = document.createElement("p");
+    foreHumid.textContent = "Humidity: " + locData.daily[i].humidity + "%";
+    foreBody.append(foreIconEl, foreTemp, foreWind, foreHumid);
+    card.append(date, foreBody);
+    forecastEl.append(card);
+  }
+  // appends all conditions/dates to their respective container/card/block
+  uvEl.append(uvCond);
+  cityNameEl.append(currDateEl, currWeatherIconEl);
+  currCityContainerEl.append(tempEl, windEl, humidityEl, uvEl);
+  // changes filled boolean to true, so checks know to call clearSpace function on another search
+  filled = true;
 }
-function storeCity(storeData){
-    var previous = {city : storeData.name, data: storeData};
-    var prevCity = JSON.parse(localStorage.getItem("City"));
-    if(prevCity === null){
-        storedCities.unshift(previous);
-        localStorage.setItem("City", JSON.stringify(storedCities));
-        var prevSearch = document.createElement("button");
-        prevSearch.textContent = storeData.name + ", " + storeData.data.sys.country;
-        prevSearch.classList = "btn bg-secondary";
-        prevSearch.setAttribute("name", storeData.name);
-        prevEl.append(prevSearch);
-        prevSearch.addEventListener("click", searchAgain);
+// function to store previous searches locally
+function storeCity(storeData) {
+  // object of current city name and data from api url
+  var previous = { city: storeData.name, data: storeData };
+  // previous searches stored locally, converts from JSON string object back to usable object
+  var prevCity = JSON.parse(localStorage.getItem("City"));
+  // test to see if anything is stored if not adds city/data to array in local storage with key City, then calls createPrev function
+  if (prevCity === null) {
+    storedCities.unshift(previous);
+    localStorage.setItem("City", JSON.stringify(storedCities));
+    createPrev(previous);
+  }
+  // if not empty loops through stored cities and compares current coordinates to each stored city's coordinates
+  else {
+    var noRepeat = true;
+    for (i = 0; i < prevCity.length; i++) {
+      if (
+        prevCity[i].data.coord.lat == previous.data.coord.lat &&
+        prevCity[i].data.coord.lon == previous.data.coord.lon
+      ) {
+        // if coordinates match changes boolean to false indicating a its already been stored and not to create another button
+        noRepeat = false;
+      }
     }
-    else{
-        var noRepeat = true;
-        for (i=0;i<prevCity.length;i++){
-            console.log(prevCity[i].data.coord);
-            if(prevCity[i].data.coord.lat == previous.data.coord.lat && prevCity[i].data.coord.lon == previous.data.coord.lon){
-            console.log("same city searched");
-            noRepeat=false;
-            }
-        }
-        if(noRepeat ===true){
-            storedCities.unshift(previous);
-            localStorage.setItem("City", JSON.stringify(storedCities));
-            var prevSearch = document.createElement("button");
-            prevSearch.textContent = storeData.name + ", " + storeData.data.sys.country;
-            prevSearch.classList = "btn bg-secondary";
-            prevSearch.setAttribute("name", storeData.name);
-            prevEl.append(prevSearch);
-            prevSearch.addEventListener("click", searchAgain);
-        }
+    // if boolean is true adds city/data to array in local storage, then calls createPrev functions
+    if (noRepeat === true) {
+      storedCities.unshift(previous);
+      localStorage.setItem("City", JSON.stringify(storedCities));
+      createPrev(previous);
     }
-    
-}  
-function searchAgain(event){
-    event.preventDefault();
-    // console.log(this.name);
-    var prevCity = JSON.parse(localStorage.getItem("City"));
-    // console.log(prevCity);
-    for(i=0;i<prevCity.length; i++){
-        if(prevCity[i].city == this.name){
-            console.log(i);
-            console.log(prevCity[i]);
-            if(filled == true){
-            clearSpace();
-            }
-            getCoordinates(prevCity[i].data,prevCity[i].city)
-        }
-    }
+  }
 }
-function init(){
-    var oldCities = JSON.parse(localStorage.getItem("City"));
-    if(oldCities !== null){
-        storedCities = oldCities;
-        for(i=0;i<storedCities.length;i++){
-        var prevSearch = document.createElement("button");
-        prevSearch.textContent = storedCities[i].city + ", " + storedCities[i].data.sys.country;
-        prevSearch.classList = "btn bg-secondary";
-        prevSearch.setAttribute("name", storedCities[i].city);
-        prevSearch.addEventListener("click", searchAgain);
-        prevEl.append(prevSearch);
-        }
+// function accepts button click on previously searched city and displays current/forecast weather
+function searchAgain(event) {
+  event.preventDefault();
+  var prevCity = JSON.parse(localStorage.getItem("City"));
+  // loops to check which button was clicked, checks if current city container is filled, and gives correct city name to getCoordinates function
+  for (i = 0; i < prevCity.length; i++) {
+    if (prevCity[i].city == this.name) {
+      if (filled == true) {
+        clearSpace();
+      }
+      getCoordinates(prevCity[i].data);
     }
+  }
 }
-
-function clearSpace(){
-    while(currCityContainerEl.firstChild) {
-        currCityContainerEl.removeChild(currCityContainerEl.firstChild);
+// on page load calls createPrev for each city in local storage to build prev searched city buttons if there are any
+function init() {
+  var oldCities = JSON.parse(localStorage.getItem("City"));
+  if (oldCities !== null) {
+    storedCities = oldCities;
+    for (i = 0; i < storedCities.length; i++) {
+      createPrev(storedCities[i]);
     }
-    while(forecastEl.firstChild){
-        forecastEl.removeChild(forecastEl.firstChild);
-    }
-    currWeatherIconEl.setAttribute("src", "");
+  }
 }
+// uses loops to clear previous conditions/names in current city/forecast container
+function clearSpace() {
+  while (currCityContainerEl.firstChild) {
+    currCityContainerEl.removeChild(currCityContainerEl.firstChild);
+  }
+  while (forecastEl.firstChild) {
+    forecastEl.removeChild(forecastEl.firstChild);
+  }
+  currWeatherIconEl.setAttribute("src", "");
+}
+// builds a clickable button in the previous search element and adds name to use as comparison for searchAgain function when clicked again
+function createPrev(saveCities) {
+  var prevSearch = document.createElement("button");
+  prevSearch.textContent = saveCities.city + ", " + saveCities.data.sys.country;
+  prevSearch.classList = "btn prevBtn";
+  prevSearch.setAttribute("name", saveCities.city);
+  prevEl.append(prevSearch);
+  prevSearch.addEventListener("click", searchAgain);
+}
+// call to initialize/display local storage
 init();
+// calls handleSearchFormSubmit on search button click/enter
 searchFormEl.addEventListener("submit", handleSearchFormSubmit);
